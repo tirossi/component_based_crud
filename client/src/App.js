25bfb05e967fab87c36369597b0e829b406a33b2
tiago <tiagorferrer@hotmail.com>
2022-06-08 18:57:08 -0300

    trampo

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import { Input, Button } from "./components";
 
+const url = 'http://localhost:3001';
+
 const App = () => {
   const [movie, setMovie] = useState('');
+  const [review, setReview] = useState('');
+  const [moviesView, setMoviesView] = useState([]);
+  const [reviewsView, setReviewsView] = useState([]);
 
+  useEffect(() => {
+    readMovies();
+    readReviews();
+  }, [])
+
+  // MOVIES
   const createMovie = () => {
-    console.log(`created movie: ${movie}`);
+    axios.post(`${url}/create/movie`, { movie })
+    readMovies()
+  }
+
+  const readMovies = () => {
+    axios.get(`${url}/movies`).then((res) => {
+      setMoviesView(res.data)
+    })
+  }
+
+  const deleteMovie = (movieId) => {
+    axios.get(`${url}/delete/movie/${movieId}`)
+    readMovies();
+  }
+
+  // REVIEWS
+  const createReview = (movie) => {
+    axios.post(`${url}/create/review`, { text: review, movieName: movie })
+    readMovies()
+  }
+
+  const readReviews = () => {
+    axios.get(`${url}/reviews`).then((res) => {
+      setReviewsView(res.data)
+    })
+  }
+
+  const updateReview = () => {
+    //todo
+  }
+
+  const deleteReview = () => {
+    // todo
   }
 
   return (
@@ -19,7 +63,19 @@ const App = () => {
       </div>
       <h2>Filmes</h2>
       <div>
-
+        {moviesView.map(movie => {
+          return (
+            <div className="movie-container">
+              <h2>{movie.name}</h2>
+              <Input type="text" placeholder="review" value={review} onChange={e => setReview(e.target.value)} />
+              <Button onClick={() => createReview(movie.name)} btnText="Publicar Review" />
+              <Button onClick={() => deleteMovie(movie.id)} btnText="Deletar filme" />
+              {reviewsView.map(review => {
+                return <p>{review.text}</p>
+              })}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
--- server/server.js
+++ server/server.js
@@ -1,8 +1,74 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'sqluser',
+  password: 'password',
+  database: 'crudrafa'
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+db.connect(err => {
+  if (err) throw err;
+  console.log('mysql connection established')
+})
+
+// CREATE MOVIE
+app.post('/create/movie', (req, res) => {
+  const movie = req.body.movie;
+
+  const query = "INSERT INTO movie (NAME) VALUES (?)";
+  db.query(query, movie, (err, result) => {
+    console.log(err);
+  });
+})
+
+// READ MOVIES
+app.get('/movies', (req, res) => {
+  const query = "SELECT * FROM movie"
+  db.query(query, (err, result) => {
+    res.send(result);
+  });
+})
+
+// DELETE MOVIE
+app.get('/delete/movie/:movieId', (req, res) => {
+  const movieId = req.params.movieId;
+
+  const query = "DELETE FROM movie WHERE id = ?";
+  db.query(query, movieId, (err, result) => {
+    console.log(err);
+  })
+})
+
+// CREATE REVIEW
+app.post('/create/review', (req, res) => {
+  const movie = req.body.movieName;
+  const text = req.body.text;
+
+  const query = "INSERT INTO review (text, movieName) VALUES (?, ?)";
+  db.query(query, [text, movie], (err, result) => {
+    console.log(err);
+  });
+})
+
+// READ REVIEW
+app.get('/reviews', (req, res) => {
+  const query = "SELECT * FROM review"
+  db.query(query, (err, result) => {
+    res.send(result);
+  });
+})
 
 app.get('/', (req, res) => {
-  res.send('server running');
+  res.send('server running, go to localhost:3000 for application')
 })
 
 app.listen(3001, () => {
